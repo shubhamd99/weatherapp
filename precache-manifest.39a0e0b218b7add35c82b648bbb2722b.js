3809a2f6c282c74eb7f6d71d1a7b715a2507b34a
Shubham Dhage <shubhamdhage930@gmail.com>
2019-02-20 16:22:57 +0530

    Updates

---
+++ precache-manifest.39a0e0b218b7add35c82b648bbb2722b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c9c4f9d9305496ae5164b271ca4be03",
+    "url": "/weatherapp/static/media/bgg.8c9c4f9d.jpg"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weatherapp/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "15922b2ccbef11c55e94",
+    "url": "/weatherapp/static/js/runtime~main.15922b2c.js"
+  },
+  {
+    "revision": "52cdef76d17f269c64dd",
+    "url": "/weatherapp/static/js/main.52cdef76.chunk.js"
+  },
+  {
+    "revision": "a7503924be3e482a2e83",
+    "url": "/weatherapp/static/js/2.a7503924.chunk.js"
+  },
+  {
+    "revision": "52cdef76d17f269c64dd",
+    "url": "/weatherapp/static/css/main.9d458e5a.chunk.css"
+  },
+  {
+    "revision": "a7503924be3e482a2e83",
+    "url": "/weatherapp/static/css/2.7cf24f9f.chunk.css"
+  },
+  {
+    "revision": "2b32b089db2d2d4e4a40c52b96c0ba0d",
+    "url": "/weatherapp/index.html"
+  }
+];
