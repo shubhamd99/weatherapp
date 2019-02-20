09e0601fa08e3166f4cd2353f7d75b434e9d842c
Shubham Dhage <shubhamdhage930@gmail.com>
2019-02-20 16:10:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,89 @@
-import React, { Component } from 'react';
-import Clock from './Clock';
-import './App.css';
-import { Form, FormControl, Button } from 'react-bootstrap';
-import Footer from './Footer';
+import React from 'react';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
+/* const API_KEY = 'YOUR_API_KEY'; */
 
-// Initial State
-class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			deadline: 'December 25, 2019',
-			newDeadline: ''   //Empty string where we store user input data
-		}
+class App extends React.Component {
+	// New state method introduced in React 16
+	// Initial State
+	state = {
+		longitude: undefined,
+		lattitude: undefined,
+		temperature: undefined,
+		humidity: undefined,
+		description: undefined,
+		wind: undefined,
+		error: undefined
 	}
 
-// Updated State
-changeDeadline(){
-	
-	this.setState({ deadline: this.state.newDeadline });
-}
-
-
-	render() {
-		return (
-			<div className="App">
-
-			    <div className="App-title">Countdown to {this.state.deadline} </div>
-			    
-			   <Clock deadline={this.state.deadline} />
-
-			    <Form inline className="Form-css">
-			    	<FormControl className="Deadline-input" placeholder='new date' onChange = {event => this.setState({newDeadline: event.target.value})} />
-			    	<Button variant="danger" onClick={() => this.changeDeadline()} > Submit</Button>
-			    </Form>
-
-			    <Footer />
-
-			</div>
-		)
+	getWeather = async (e) => {
+		e.preventDefault(); // It is use to remove the whole page load when we press the button
+		const longitude = e.target.elements.longitude.value;
+		const lattitude = e.target.elements.lattitude.value;
+		const api_call = await fetch(`https://fcc-weather-api.glitch.me/api/current?lon=${longitude}&lat=${lattitude}`, {method: 'get'});
+		const data = await api_call.json();  // Converting the data into readable with JSON fetch
+		
+		if (longitude && lattitude) {
+			this.setState({
+				longitude: data.coord.lon,
+				lattitude: data.coord.lat,
+				temperature: data.main.temp,
+				humidity: data.main.humidity,
+				description: data.weather[0].description,
+				wind: data.wind.speed,
+				error: ""
+		     });
+		} else {
+			this.setState({
+				longitude: undefined,
+				lattitude: undefined,
+				temperature: undefined,
+				humidity: undefined,
+				description: undefined,
+				wind: undefined,
+				error: "Please Enter the values"
+		     });
+		}
 		
 	}
-}
+
+	render() {
+		return(
+	        <div>
+				    <div className="wrapper">
+				        <div className="main">
+				             <div className="container shadow ">
+						          <div className="row">
+						          	    <div className="col-sm-5 title-container">
+									        <Titles />
+									    </div>
+									    <div className="col-sm-7 form-container">
+											<Form getWeather={this.getWeather} />  
+											<Weather 
+											longitude={this.state.longitude}
+											lattitude={this.state.lattitude}
+											temperature={this.state.temperature}
+											humidity={this.state.humidity}
+											description={this.state.description}
+											wind={this.state.wind}
+											error={this.state.error}
+											/>
+										</div>
+								   </div>
+							 </div>
+						</div>
+					</div>
+                                   
+		   </div>
+		);
+	}
+};
+
+
 
 export default App;
+
+
+									
